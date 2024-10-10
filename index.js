b13a5f01ba20d5a6a3491ffb906ee80bbc9c8e68
Vivek Atreya <atreya.vivek9@gmail.com>
2024-10-10 18:00:30 +0530

    lecture 2

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
 //     id: "heading", xyz: "abc"
 // }, "Hello World from React!");/
 
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" },
-        [React.createElement("h1", { id: "heading" }, "I'm a H1 Tag"),
-        React.createElement("h2", { id: "heading" }, "I'm a H2 Tag")]
+        [React.createElement("h1", { id: "heading" }, "Hi There!!"),
+        React.createElement("h2", { id: "heading" }, "How you Doin'")]
     )
 )
 
